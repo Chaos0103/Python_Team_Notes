1382f068145fb367b8fbb04c2f9e88876cfc6590
chaos0103 <lyt1228@naver.com>
2022-02-07 17:27:13 +0900

    삽입 정렬

--- Sorting/Insertion_sort.py
+++ Sorting/Insertion_sort.py
@@ -1,5 +1,6 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
+''' Insertion Sort'''
 for i in range(1, len(array)):
     for j in range(i, 0, -1):  # 인덱스 i부터 1까지 감소하면서 반복하는 문법
         if array[j] < array[j-1]:  # 한 칸씩 왼쪽으로 이동
