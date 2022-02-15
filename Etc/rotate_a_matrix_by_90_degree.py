0f9a76fb99f91591087241f02e4af55a95e6c699
chaos0103 <lyt1228@naver.com>
2022-02-15 15:51:39 +0900

    2차원 리스트(행렬)를 90도 회전시키는 함수

---
+++ Etc/rotate_a_matrix_by_90_degree.py
@@ -0,0 +1,13 @@
+''' 2차원 리스트(행렬)를 90도 회전하는 메서드 '''
+
+
+def rotate_a_matrix_by_90_degree(a):
+    row_length = len(a)
+    column_length = len(a[0])
+
+    res = [[0] * row_length for _ in range(column_length)]
+    for r in range(row_length):
+        for c in range(column_length):
+            res[c][row_length - 1 - r] = a[r][c]
+
+    return res
